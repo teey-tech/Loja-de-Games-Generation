aed6a2594acac7ab389fe97f0b0cb74ff5a34941
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-01-24 15:52:59 -0300

    @BruRaptor [Adicinando Banner]

--- src/main/java/com/generation/minhaLojaDeGames/MinhaLojaDeGamesApplication.java
+++ src/main/java/com/generation/minhaLojaDeGames/MinhaLojaDeGamesApplication.java
@@ -8,6 +8,7 @@ public class MinhaLojaDeGamesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhaLojaDeGamesApplication.class, args);
+		
+		System.out.println("\n Loja de Games Online!");
 	}
-
 }
