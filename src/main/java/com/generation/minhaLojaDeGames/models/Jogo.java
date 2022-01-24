3ade6804e99dff03c08b7060f9fa74d51c6eef70
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-01-24 10:39:22 -0300

    @BruRaptor[Adicionando model de Jogo]

--- src/main/java/com/generation/minhaLojaDeGames/models/Jogo.java
+++ src/main/java/com/generation/minhaLojaDeGames/models/Jogo.java
@@ -1,5 +1,87 @@
 package com.generation.minhaLojaDeGames.models;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_jogos")
 public class Jogo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(max = 1000)
+	private String nome_jogo;
+
+	@NotNull
+	@Size(max = 1000)
+	private String descricao;
+
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
+	private Date data_lancamento;
+
+	@ManyToOne
+	@JoinColumn(name = "fk_categoria")
+	@JsonIgnoreProperties("categoria")
+
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome_jogo() {
+		return nome_jogo;
+	}
+
+	public void setNome_jogo(String nome_jogo) {
+		this.nome_jogo = nome_jogo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData_lancamento() {
+		return data_lancamento;
+	}
+
+	public void setData_lancamento(Date data_lancamento) {
+		this.data_lancamento = data_lancamento;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
