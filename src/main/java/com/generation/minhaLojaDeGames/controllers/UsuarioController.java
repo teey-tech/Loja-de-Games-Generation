2717cd760e18a6db4f1827edbd7fcae79aa9d00d
Sarah Nani <sarahlenani@gmail.com>
2022-01-31 11:24:56 -0300

    @sarahnani [Adicionando método get e post no UsuarioController]

---
+++ src/main/java/com/generation/minhaLojaDeGames/controllers/UsuarioController.java
@@ -0,0 +1,56 @@
+package com.generation.minhaLojaDeGames.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.minhaLojaDeGames.models.Usuario;
+import com.generation.minhaLojaDeGames.models.UsuarioLogin;
+import com.generation.minhaLojaDeGames.repositories.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
+	}
+
+}
