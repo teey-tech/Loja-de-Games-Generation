239194ff96e91f0eaa918039103b5637030e1c98
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-01-31 11:35:47 -0300

    @BruRaptor [Adicionando BasicSecurityConfig]

--- src/main/java/com/generation/minhaLojaDeGames/seguranca/BasicSecurityConfig.java
+++ src/main/java/com/generation/minhaLojaDeGames/seguranca/BasicSecurityConfig.java
@@ -1,6 +1,30 @@
 package com.generation.minhaLojaDeGames.seguranca;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class BasicSecurityConfig {
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
     
+    @Autowired
+    private void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar").permitAll().anyRequest().authenticated().and().httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
+    }
 }
